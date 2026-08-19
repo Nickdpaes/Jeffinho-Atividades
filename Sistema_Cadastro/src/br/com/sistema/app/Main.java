8261d929caaa0a9a569548c69a7410e0a5c0a4f1
Nicollas Dom Paes <nicollasdominguespaes@gmail.com>
2026-08-19 09:15:18 -0300

    Errei, fui mlk jeff
    Esqueci de commitar a Main

--- Sistema_Cadastro/src/br/com/sistema/app/Main.java
+++ Sistema_Cadastro/src/br/com/sistema/app/Main.java
@@ -1,5 +1,25 @@
 package br.com.sistema.app;
 
+import br.com.sistema.view.TelaCliente;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Main {
 
+  public static void main(String[] args) {
+    // Toda interface Swing deve nascer na Event Dispatch Thread (EDT).
+    SwingUtilities.invokeLater(
+      new Runnable() {
+        public void run() {
+          try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+          } catch (Exception e) {
+            // O visual padrao continua funcional se o Look and Feel falhar.
+            e.printStackTrace();
+          }
+          new TelaCliente().setVisible(true);
+        }
+      }
+    );
+  }
 }
